ad20ce7b6be27c5a26652e57e7790bbe638812a8
Sourish Chakraborty <sourish1978@gmail.com>
2021-07-21 23:12:34 +0800

    test file added

---
+++ deck_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected Four of Clubs, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	ld := newDeckFromFile("_decktesting")
+	if len(ld) != 16 {
+		t.Errorf("Expected deck length of 20, but got %v", len(ld))
+	}
+
+	os.Remove("_decktesting")
+}
